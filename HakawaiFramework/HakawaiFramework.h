15ab16753fb43f61d8de69f74fade59288a6d53e
Radek Czemerys <rc@timeular.com>
2017-10-08 23:07:31 +0200

    Add framework target

---
+++ HakawaiFramework/HakawaiFramework.h
@@ -0,0 +1,19 @@
+//
+//  HakawaiFramework.h
+//  HakawaiFramework
+//
+//  Created by Radoslaw Czemerys on 08/10/2017.
+//  Copyright © 2017 LinkedIn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HakawaiFramework.
+FOUNDATION_EXPORT double HakawaiFrameworkVersionNumber;
+
+//! Project version string for HakawaiFramework.
+FOUNDATION_EXPORT const unsigned char HakawaiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HakawaiFramework/PublicHeader.h>
+
+
